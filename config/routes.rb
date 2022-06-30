a1a776a8c4d991a90f182cc126986dbd6de2b198
David Young <da.young@f5.com>
2022-06-29 22:20:40 -0500

    Minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'boards#index'
+
   resources :boards, param: :short_name do
     resources :comment_threads, only: %i[show create destroy], path: 'threads' do
       shallow { resources :comments, only: %i[destroy create] }
@@ -11,13 +13,7 @@ Rails.application.routes.draw do
     delete 'sign_out', to: 'users/sessions#destroy'
   end
 
-  root 'boards#index'
-
   authenticate :user, ->(user) { user.min_role?(:owner) } do
     mount Sidekiq::Web => '/sidekiq'
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
